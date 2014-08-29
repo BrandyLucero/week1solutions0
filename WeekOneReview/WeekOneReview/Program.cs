2bba80efbf6a43a55af9f2bdb73b7fc8d41b917e
Brandy C. Lucero <brandy.c.lucero@gmail.com>
2014-08-29 01:17:28 -0600

    Updated review

--- WeekOneReview/WeekOneReview/Program.cs
+++ WeekOneReview/WeekOneReview/Program.cs
@@ -8,22 +8,33 @@ namespace WeekOneReview
 {
     class Program
     {
+        //declare a global variable to hold the total number of loops executed
+
         static int loopCount = 0;
 
         static void Main(string[] args)
         {
-           
-            //declare a global variable to hold the total number of loops executed
-
 
             //call stringbuilding function
+
             StringBuilding("Brandy");
 
-            sportsList();
+            //print out the number of loops
+            Console.WriteLine(loopCount);
 
+            // call the string building function to do more loops
+            StringBuilding("running a few more loops");
 
-            // keep the console open
+            //print out the loops performed
+            Console.WriteLine(loopCount);
+
+            //call the list practice function 
+            ListPractice();
+
+            // keep the console open 
             Console.ReadKey();
+
+
 
         }
 
@@ -32,14 +43,14 @@ namespace WeekOneReview
         //and all constatnts lowercase.
         static void StringBuilding(string input)
         {
+            //declare a string to hold our output
+
             string output = string.Empty;
 
             // loop over the input, to compare each letter
             for (int i = 0; i < input.Length; i++)
-                 
-
             {
-               
+
                 //get a letter from input
                 //convert the char to a string
                 string letter = input[i].ToString();
@@ -50,39 +61,53 @@ namespace WeekOneReview
                 {
                     //it is a vowel add to our output and upper case letter 
                     output = output + letter.ToUpper();
-                    loopCount = loopCount + 1();
-
-
                 }
                 else
                 {
+                    //if its nor a vowel add a lower case letter to output
                     output = output + letter.ToLower();
-
                 }
-               
-                
+                //we did a loop hip hip hooray
+                //add one to loop counter
+                loopCount = loopCount + 1;
             }
+            //after the loop print the output
             //Print the output
             Console.WriteLine(output);
-             
-        }
-    
-    
-    
-        // declare a new list
-        List<string> sportsList = new List<string>(); {"baseball", "basketball", "football"}
-    //print out the contents of the sportsList using a for loop in asending order
-    Console.WriteLine(sportsList[i]
 
-{
-    for (int i = sportsList.Count - 1; i >= 0; i--)
+        }
+        //working with lists
+        static void ListPractice()
+        {
+          
+        // declare a new list of strings
+        List<string> sportsList = new List<string>() { "Baseball", "Basketball", "Football" };
+
+    //print out the contents of the sportsList using a for loop in asending order
+            for (int i = 0; i < sportsList.Count; i++)
+			{
+			 
+        Console.WriteLine(sportsList[i]);
+                // did a loop , count it on the loop count glabal variable
+                loopCount ++;
+            }
+            //List out the contents of the sports list using foreach loop               
    
-    foreach (var item in sportsList)
-	{
-		Console.WriteLine(item);
+         foreach (var item in sportsList)
+	       {
+		        Console.WriteLine(item);
  
-	}
+	      }
+            //looping over a string using for each loop
+            string whateves = "Party On People";
+            foreach (char item in whateves)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
 }
+
 
     
 
